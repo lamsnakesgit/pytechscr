96d82ccc6a6fdca08a3230a2afdfe0bfd950a21e
owner mc <solnysko@MacBook-Pro-Solnysko.local>
2021-09-16 00:18:28 +0300

    date + description displayed

--- loadop.py
+++ loadop.py
@@ -2,37 +2,24 @@ import json
 import datetime
 #import dateutil
 
-#def convert_date():
+def convert_date(i):
+    datetimeobj = datetime.datetime.strptime(i['date'],"%Y-%m-%dT%H:%M:%S.%f")
+    d = datetimeobj.strftime("%d.%m.%Y")
+    #print(d,'\n')
+    return(d)
    
 with open('operations.json') as file:
     data = json.load(file)
     #op = json.load('operations.json')
-#print(len(data)) #101 
-#print(data[:-5]) #5 times?
-#print(data[::-5])
-#print(data[:-5:])
-##print(data[-5:]) # printf x-5,x-4.. until last
 # iterate through list state from last: find 5 'executed'
 # what is 'last'? defined by time or place in list?
 # look at 'date' fields
-#print(data[-1]['date'])
-c = 0
+# format: date + desc + '\n' + from -> to \n + opAm=am+currency
+c = 1
 for i in reversed(data):
-# #for i in len(data):
-    #print(i)
     if i:
-        if i['state'] == 'EXECUTED': #85 op
-            #convert_date(i)
-            #datetimeobj = datetime.datetime.strptime(i['date'],"%Y-%M-%dT%HH:%MM:%SS.%f")
-            #datetimeobj = datetime.datetime.strptime(i['date'],"%Y-%M-%d")
-            datetimeobj = datetime.datetime.strptime(i['date'],"%Y-%m-%dT%H:%M:%S.%f")
-            ###d = dateutil.parser.parse(i['date'])
-            ###print("DUT\n", d, "\n")
-            ##i['date'].isoformat()
-          #  print(i['date'], "\n")
-            #d = datetimeobj.strftime("%D.%M.%Y") #wtf is with D M
-            d = datetimeobj.strftime("%d.%m.%Y")
-            print(d,'\n')
-            #print(i["date"]) #debugged
-            #print(i["id"])
+        if i['state'] == 'EXECUTED' and c <= 5: #85 op
+            dt = convert_date(i)
+            print(dt, i['description'])
+            print()
             c += 1
