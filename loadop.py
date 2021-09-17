453a2b95d0ab2cf485796aee4978c052242f4e90
owner mc <solnysko@MacBook-Pro-Solnysko.local>
2021-09-17 17:19:57 +0300

    cleaner version

--- loadop.py
+++ loadop.py
@@ -1,16 +1,11 @@
 import json
 import datetime
 import re
-#import dateutil
 
 def convert_date(i):
     datetimeobj = datetime.datetime.strptime(i['date'],"%Y-%m-%dT%H:%M:%S.%f")
     d = datetimeobj.strftime("%d.%m.%Y")
-    #print(d,'\n')
     return(d)
-# amount of digits from end, then if "СЧЕТ" or eng
-# if start substring = "Счет"
-# find first digits first for cards
 
 def iscardoracc(src):
     if 'Счет' in src:
@@ -22,28 +17,17 @@ def iscardoracc(src):
         if m is not None:
             num_index = m.start()
             card = src[num_index:num_index + 4:] + " " + src[num_index + 4:num_index + 6:] + 2 * "*" + " " + 4 * "*" + " "+ src[:-5:-1]
-        return(card) #last 4 digits
-#    for a in reversed(i):
-#        print(a)
-#    while i['from'][::-1].isdigit():
-#        print(i['from'][::-1].isdigit())
+        return(card)
    
 with open('operations.json') as file:
     data = json.load(file)
-    #op = json.load('operations.json')
-# iterate through list state from last: find 5 'executed'
-# what is 'last'? defined by time or place in list?
-# look at 'date' fields
-# format: date + desc + '\n' + from -> to \n + opAm=am+currency
-# if account = elif card = frmt 
+
 c = 1
 for i in reversed(data):
     if i:
-        if i['state'] == 'EXECUTED' and c <= 5: #85 op
+        if i['state'] == 'EXECUTED' and c <= 5:
             dt = convert_date(i)
             print(dt, i['description'])
-            # FROM -> TO card block by 4
-            # SUM CURRENCY
             if 'from' in i:
                 src = iscardoracc(i['from'])
             else:
@@ -51,7 +35,6 @@ for i in reversed(data):
             if 'to' in i:
                 src2 = iscardoracc(i['to'])
             print(src, src2)
-            # SUM CURRENCE
             print(i['operationAmount']['amount'], i['operationAmount']['currency']['name'])
             print()
             c += 1
